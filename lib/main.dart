335f82b57dee43125775a55c17ea49733639e6a0
code-raushan <code-raushan@users.noreply.github.com>
2024-04-13 22:32:22 +0530

    add: notes addition feature added

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,79 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Notes application",
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Notes Application",
-            style: TextStyle(
-              color: Colors.white,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _textEditingController = TextEditingController();
+  List<String> notes = [];
+
+  void _showAddNoteModal(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            title: const Text("Add your note"),
+            content: TextField(
+              controller: _textEditingController,
+              decoration: const InputDecoration(
+                hintText: "Enter your note",
+              ),
             ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    notes.add(_textEditingController.text);
+                  });
+                  _textEditingController.clear();
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Add Note"),
+              )
+            ]);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        title: const Text(
+          "Notes Taker",
+          style: TextStyle(
+            color: Colors.white,
           ),
-          backgroundColor: const Color.fromARGB(255, 6, 31, 51),
         ),
-        body: const Center(
-          child: Text(
-            "Welcome to the Notes application",
-            style: TextStyle(color: Colors.green, fontSize: 15),
-          ),
-        ),
+      ),
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(notes[index]),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddNoteModal(context);
+        },
+        backgroundColor: Colors.amber,
+        child: const Icon(Icons.add),
       ),
     );
   }
