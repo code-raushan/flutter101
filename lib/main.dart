b9f1605130c90ef504c3606c30b0756684ee1407
code-raushan <code-raushan@users.noreply.github.com>
2024-04-13 21:49:00 +0530

    project init

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Notes application",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Notes Application",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 6, 31, 51),
+        ),
+        body: const Center(
+          child: Text(
+            "Hi",
+            style: TextStyle(color: Colors.green, fontSize: 30),
+          ),
+        ),
+      ),
+    );
+  }
+}
